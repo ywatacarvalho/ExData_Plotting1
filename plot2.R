4b02d9d45372b9b73e4222370a64e77e8e90cfbe
ywatacarvalho <ywatacarvalho@users.noreply.github.com>
2014-08-09 01:21:32 -0300

    plot2.R
    Program for file plot2.png

---
+++ plot2.R
@@ -0,0 +1,26 @@
+
+rm(list=ls());
+
+setwd("C:/Alex/AulasWEB/ExploratoryDataAnalysis/Project_1");
+
+power <- read.table("household_power_consumption.txt", sep=";", header=TRUE, dec = ".",
+                    colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric"),
+                    na.strings = c("?"));
+
+power$datetime <- strptime(paste(power$Date, power$Time), "%d/%m/%Y %H:%M:%S");
+power$date <- strptime(power$Date, "%d/%m/%Y");
+
+summary(power);
+
+power1 <- subset(power, as.Date(date) <= as.Date('2007-02-02') & as.Date(date) >= as.Date('2007-02-01'))
+
+summary(power1);
+
+Sys.setlocale("LC_TIME", "English");
+
+with(power1, plot(datetime, Global_active_power, type="o", pch='', xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+png(file = "plot2.png", width = 480, height = 480, units = "px");
+with(power1, plot(datetime, Global_active_power, type="o", pch='', xlab = "", ylab = "Global Active Power (kilowatts)"))
+dev.off();
+
